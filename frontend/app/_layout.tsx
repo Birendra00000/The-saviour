e823cd8e7648f8e1932b57328c6fda2397a6e52d
Birendra <bibekbhusal400@gmail.com>
2024-12-05 10:00:00 +0545

    created all components needed for Flood sos pages

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -38,6 +38,41 @@ export default function RootLayout() {
 
         <Stack.Screen name="/auth/Login" options={{ headerShown: false }} />
         <Stack.Screen name="/page/Ambulance" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="/page/Menu/RegisterNumber"
+          options={{
+            headerShown: true, // Show the header if desired
+            title: "Register Number", // Custom title
+          }}
+        />
+        <Stack.Screen
+          name="/page/Menu/ViewMember"
+          options={{
+            headerShown: true, // Show the header if desired
+            title: "View Number", // Custom title
+          }}
+        />
+        <Stack.Screen
+          name="/page/Menu/DeleteNumber"
+          options={{
+            headerShown: true, // Show the header if desired
+            title: "View Number", // Custom title
+          }}
+        />
+        <Stack.Screen
+          name="/page/Menu/EditSoSMessage"
+          options={{
+            headerShown: true, // Show the header if desired
+            title: "View Number", // Custom title
+          }}
+        />
+        <Stack.Screen
+          name="/page/Menu/EditTimer"
+          options={{
+            headerShown: true, // Show the header if desired
+            title: "View Number", // Custom title
+          }}
+        />
 
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
